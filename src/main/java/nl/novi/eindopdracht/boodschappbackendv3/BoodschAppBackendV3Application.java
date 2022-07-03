e9d88e59c9b64796dfde21bd78a1e5a2e96b3634
BayramaliDilek <bayramalidilek@gmail.com>
2022-07-03 13:23:05 +0200

    Eerste commit, start van de web-app.

---
+++ src/main/java/nl/novi/eindopdracht/boodschappbackendv3/BoodschAppBackendV3Application.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht.boodschappbackendv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoodschAppBackendV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoodschAppBackendV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/eindopdracht/boodschappbackendv3/BoodschAppBackendV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht.boodschappbackendv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoodschAppBackendV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
